9cee9fd272fda403ec0de273722572354fcf146e
Kaigh Taylor <coders@kai-deas.com>
2022-01-26 20:58:43 -0500

    JSBB _Person | object literals added

---
+++ JSBB_Person/script.js
@@ -0,0 +1,13 @@
+/* jshint esversion: 8 */
+
+// Chanllenge
+// build a person with object literal notation
+// use thredd property values consisting of a string, number, and boolean
+
+// possible solution
+
+var thisPerson = {
+    name : 'Timmy',
+    age : 4,
+    employed: false
+};
