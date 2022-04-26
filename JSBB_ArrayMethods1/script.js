74827e59bf2b0bacc507995926c3ca618a1c0e22
Kaigh Taylor <coders@kai-deas.com>
2022-04-25 21:51:39 -0400

    JSBB ArrayMethods | unshift push

---
+++ JSBB_ArrayMethods1/script.js
@@ -0,0 +1,17 @@
+// array methods review
+// push() pop() shift() unshift()
+
+// practice
+var shoppingList = ['apples', 'pizza', 'chicken', 'mushrooms', 'oranges', 'beef'];
+console.log('The shoppingList array consists of the following: ' + shoppingList); 
+// challengekk
+// insert 'crackers' at the front of the array and 'grapes' at the end of the array
+
+
+console.log('Using unshift method to add an element to the front of the shoppingList array') ;
+shoppingList.unshift('crakers');
+console.log('The shoppingList array now consists of the following: ' + shoppingList);
+
+console.log('\nUsing push method to add an element to the end of the shoppingList array') ;
+shoppingList.push('grapes');
+console.log('The shoppingList array now consists of the following: ' + shoppingList);
