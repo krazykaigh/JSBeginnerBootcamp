54e6c5a1a07f3007f86e17c8fe1ae91a8acaa628
Kaigh Taylor <coders@kai-deas.com>
2022-05-07 16:33:33 -0400

    JSBB_Fns | Added Params

---
+++ JSBB_Functions/script.js
@@ -0,0 +1,18 @@
+// Create first function    
+
+function morningGreeting(firstName) {
+    return 'Good morning, ' + firstName + '. I hope you are well. \nThank you for everything';
+}
+
+console.log(morningGreeting('Jim'));
+
+// Challenge 
+// write function to include a parameter and arguments for name 
+// replace everything with something specific
+
+function morningEvening(firstName, everything) {
+    return 'Good evening, ' + firstName + '. \nThank you for ' + everything + '.';
+}
+
+console.log(morningEvening('Jim', 'walking my dog while I was away'));
+
