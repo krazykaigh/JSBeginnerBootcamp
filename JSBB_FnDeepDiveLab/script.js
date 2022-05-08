8f07881a39e461593494231d62871890eab3fe2a
Kaigh Taylor <coders@kai-deas.com>
2022-05-08 12:44:26 -0400

    JSBB_FnDDL | .map usage

--- JSBB_FnDeepDiveLab/script.js
+++ JSBB_FnDeepDiveLab/script.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 8 */
+
 // Review
 // function expressions
 // anonymous expressions
@@ -11,7 +13,7 @@ function knockKnock() {
 // Challenge
 // call the knockKnock function
 
-console.log(knockKnock());
+console.log('1. ' + knockKnock());
 
 // challenge
 // turn knockKnock into an ANONYMOUS FUNCTION with a function expression
@@ -21,8 +23,8 @@ var fnKnockKnock = function() {
     return 'Who\'s there?';
 };
 
-console.log('fnKnockKnock\n' +  fnKnockKnock);
-console.log(fnKnockKnock());
+console.log('2. fnKnockKnock\n' +  fnKnockKnock);
+console.log('3. ' + fnKnockKnock());
 
 // immediately Invoked Function Expression (IFFE)
 // practice
@@ -31,11 +33,36 @@ function dogWalker(person, dog) {
     return person + ' is walking a ' + dog;
 }
 
-console.log(dogWalker('Paul', 'minpin'));
-
+console.log('4. ' + dogWalker('Paul', 'minpin'));
+var person = 'Mark';
+var breed = 'Dobbermin';
 // dogWalker turned into an IIFE
-console.log((function(person, dog) {
+var iffeDogWalker = (function(person, dog) {
     return person + ' is walking a ' + dog;
-}('Mike', 'sharpe')));
+}(person, breed));
+
+console.log('5. ' + iffeDogWalker); // compare to 2. calling name without () invokes function
+
+let factory = {
+    items: [5, 1, 12],
+    double: function(){
+        return this.items.map((item, index) => {
+            let value = item*2;
+            console.log(`${value} is the double of ${this.items[index]}`);
+            return value;
+        });
+    }
+};
+
+factory.double();
+
+let arr = [3, 4, 5, 6];
+
+let modifiedArr = arr.map(function(element){
+    return element *3.5;
+});
+
+console.log(modifiedArr);
+
 
 
