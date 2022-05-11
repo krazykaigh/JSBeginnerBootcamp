3cd7fca659568f243428f28bf75b8f3e1b4d8e2d
Kaigh Taylor <coders@kai-deas.com>
2022-05-11 16:26:08 -0400

    JSBB_BHG | switch update

--- bearHumanGun/script.js
+++ bearHumanGun/script.js
@@ -3,42 +3,45 @@
 // 1. Get the user's choice.
 console.log('Please choose your power: ');
 var choice = prompt('\n1. Bear\n2. Human\n3. Gun' );
-var choiceName;
+var choiceName = 'CHANGE ME';
 console.log('You picked number :' + choice);2
 switch(choice) {
     case "1":
         choiceName = "Bear";
-        console.log('\nYou chose a ' + choiceName);
+        console.log('You chose a ' + choiceName);
         break;
     case "2":
         choiceName = "Human";
-        console.log('\nYou chose a ' + choiceName);
+        console.log('You chose a ' + choiceName);
         break;
     case "3":
         choiceName = "Gun";
-        console.log('\nYou chose a ' + choiceName);
+        console.log('You chose a ' + choiceName);
         break;
 }
 
 // 2. Get the computer's choice.
 var compChoice =  Math.floor(Math.random() *3 ) + 1;
+var compChoiceName = '';
 console.log('The computer picked number: ' + compChoice);
-var compChoiceName;
 switch(compChoice) {
     case "1":
         compChoiceName = "Bear";
         console.log('The computer chose a ' + compChoiceName);
+        console.log("AT BEAR COMPCHOICE")
         break;
     case "2":
         compChoiceName = "Human";
+        console.log("AT HUMAN COMPCHOICE")
         console.log('The computer chose a ' + compChoiceName);
         break;
     case "3":
         compChoiceName = "Gun";
+        console.log("AT GUN COMPCHOICE")
         console.log('The computer chose a ' + compChoiceName);
         break;
 }
-var BGH = function(choice, compChoice) {
+var BGH = function(choice, choiceName, compChoice, compChoiceName) {
 if (choice == compChoice) {
     return 'Tie! You both picked: ' + choiceName;
 } else if ((choice == 1) && (compChoice == 2)) {
@@ -56,5 +59,5 @@ if (choice == compChoice) {
 // 3. Compare the two choices and determine a winner.
 
 // 4l. Start the program and display the results. 
-console.log(BGH(choice, compChoice));
+console.log(BGH(choice, choiceName, compChoice, compChoiceName));
 
